fd3d97562dbeb4d8c5e95855df4bbd4fa8379c2a
WilliamGlynn <ryuwill@sbcglobal.net>
2024-02-22 19:33:16 -0600

    Got javascript file to load for report preview

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/css', express.static(path.join(__dirname, 'css')));
 
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
