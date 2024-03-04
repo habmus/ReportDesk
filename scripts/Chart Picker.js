98ee6162f57717ba273f7df073fa34eb980c8e1a
WilliamGlynn <ryuwill@sbcglobal.net>
2024-03-04 12:34:26 -0600

    added charts

--- scripts/Chart Picker.js
+++ scripts/Chart Picker.js
@@ -1,6 +1,36 @@
-//dailyChart(50, 51);
+fetch('/api/reportType')
+    .then(response => response.text())
+    .then(type => console.log(type));
 
-
-const months = ["January", "February", "March", "April", "May", "June"];
-const questions = [1000, 1260, 8000, 4021, 5993, 10000];
-monthlyChart(months, questions);
+let x = text;
+chartPicker(x)
+function chartPicker(pick) {
+switch (pick)
+{
+    case 1:
+        dailyChart(50, 51);
+        break;
+    case 2:
+        const months = ["January", "February", "March", "April", "May", "June"];
+        const questions = [1000, 1260, 8000, 4021, 5993, 10000];
+        monthlyChart(months, questions);
+        break;
+    case 3:
+        const questionsPerHour = [80,70,6000,5504,100,6000,900,300,1000,700,8000,412,600]
+        yearlyChart (questionsPerHour);
+        break;
+    case 4:
+        durationChart();
+        break;
+    case 5:
+        locationChart();;
+        break;
+    case 6:
+        const mCourse =["CSCE 1301", "CJUS 2600", "CHEM 3452", "SPAN 4321", "BCIS 3610",];
+        const mQuestions =[125, 100, 87, 75, 61,];
+        coursesChart(mCourse, mQuestions); 
+        break;
+    default:
+        "error"
+}
+}
---
+++ scripts/Yearly Chart.js
@@ -0,0 +1,40 @@
+function yearlyChart(questionsPerHour)
+{
+    const xValues = ["8:00AM","9:00AM","10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM", "6:00PM", "7:00PM", "8:00PM"] 
+    const yValues = questionsPerHour
+    
+    new Chart("myChart", {
+      type: "line",
+      data: {
+        labels: xValues,
+        datasets: [{
+          fill: false,
+          lineTension: 0,
+          backgroundColor:"rgba(0,0,255,1.0)",
+          borderColor: "Black",
+          data: yValues
+        }]
+      },
+      options:
+      { 
+        legend: {display: false},
+        elements: {point: {radius: 6}}
+      }
+    });
+    var table ="<table><tr><th>Hour</th><th>Total Traffic<th></tr>"+
+    "<tr>"+ "<td>" +xValues[0] + "</td><td>" + yValues[0] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[1] + "</td><td>" + yValues[1] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[2] + "</td><td>" + yValues[2] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[3] + "</td><td>" + yValues[3] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[4] + "</td><td>" + yValues[4] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[5] + "</td><td>" + yValues[5] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[6] + "</td><td>" + yValues[6] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[7] + "</td><td>" + yValues[7] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[8] + "</td><td>" + yValues[8] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[9] + "</td><td>" + yValues[9] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[10] + "</td><td>" + yValues[10] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[11] + "</td><td>" + yValues[11] + "</td></tr>" +
+    "<tr>"+ "<td>" +xValues[12] + "</td><td>" + yValues[12] + "</td></tr>"
+    + "</table>";
+    document.querySelector('[title="Table"]').innerHTML = table; 
+}
